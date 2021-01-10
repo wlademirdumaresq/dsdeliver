348ef6531e52677a002616e1ad9a7b3a46c814b4
wlademirdumaresq <wlademir.dumaresq@gmail.com>
2021-01-09 22:14:26 -0300

    ajuste de variaveis de ambiente

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/Type";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
+
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
